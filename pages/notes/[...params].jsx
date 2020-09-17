ecd31abc11d65c5f9f9c1cb399c286fe11045f6b
Atik Fahad <fahadatik@gmail.com>
2020-09-17 06:52:21 +0600

    routing

---
+++ pages/notes/[...params].jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import { useRouter } from 'next/router';
+
+const Page = () => {
+  const router = useRouter();
+  // notes/1/2/3/4
+  const { params } = router.query;
+  console.log(params);
+  return <div>Note {params}</div>;
+};
+export default Page;
